f3588cc261545327e7786d3c734c3c1ab822be6f
고유창 <go95305@naver.com>
2020-10-27 02:54:40 +0900

    20/10/26

--- SSAFY_Algorithm/src/algorithm/B16236_아기상어.java
+++ SSAFY_Algorithm/src/algorithm/B16236_아기상어.java
@@ -1,106 +1,125 @@
 package algorithm;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
 
 public class B16236_아기상어 {
 	static int N;
+	static ArrayList<Point> list;
+	static ArrayList<Point> fishlist;
 	static int map[][];
-	static int babySize = 0;
-	static boolean v[][][];
 	static int dr[] = { -1, 1, 0, 0 };
 	static int dc[] = { 0, 0, -1, 1 };
 	static int Ans;
-	static int fish;
+	static int sharkSize;
+	static int sharkRN;
+	static int sharkCN;
+	static boolean v[][];
+	static int move;
+	static int minR, minC, minIdx, minCnt;
 
 	static class Point {
-		@Override
-		public String toString() {
-			return "Point [r=" + r + ", c=" + c + ", cnt=" + cnt + ", eat=" + eat + ", visit=" + visit + "]";
-		}
+		int r, c, size, cnt;
 
-		int r, c, cnt, eat, visit;
-
-		public Point(int r, int c, int cnt, int eat, int visit) {
-			super();
+		Point(int r, int c, int size, int cnt) {
 			this.r = r;
 			this.c = c;
+			this.size = size;
 			this.cnt = cnt;
-			this.eat = eat;
-			this.visit = visit;
 		}
-
 	}
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		N = sc.nextInt();
 		map = new int[N][N];
-		v = new boolean[100][N][N];
-		int stx = 0;
-		int sty = 0;
-		fish = 0;
+		list = new ArrayList<Point>();
+
+		sharkRN = 0;
+		sharkCN = 0;
+		int eaten=0;
+		sharkSize = 2;
 		for (int i = 0; i < N; i++) {
 			for (int j = 0; j < N; j++) {
 				map[i][j] = sc.nextInt();
-				if (map[i][j] > 0 && map[i][j] != 9) {
-					fish++;
-				}
 				if (map[i][j] == 9) {
-					stx = i;
-					sty = j;
-					map[i][j]=0;
+					sharkRN = i;
+					sharkCN = j;
+				}
+				if (map[i][j] > 0 && map[i][j] < 9) {
+					list.add(new Point(i, j, map[i][j], 0));
 				}
 			}
 		}
-
-		babySize = 2;// 초기 아기상어 크기
-		Ans = Integer.MAX_VALUE;
-
-		bfs(stx, sty, 0);
-		System.out.println(Ans);
+		minR = minC = minIdx = 0;
+		move = 0;
+		int dist=0;
+		while (!list.isEmpty()) {
+			minCnt = Integer.MAX_VALUE;
+			for (int i = 0; i < list.size(); i++) {
+				v = new boolean[N][N];		
+				if (list.get(i).size < sharkSize) {
+					bfs(list.get(i).r, list.get(i).c,i);
+				}
+			}
+			if (minCnt == Integer.MAX_VALUE)
+				break;
+			map[sharkRN][sharkCN]=0;
+			sharkRN=minR;
+			sharkCN=minC;
+			eaten++;
+			if (eaten == sharkSize) {
+				sharkSize++;
+				eaten=0;
+			}
+			dist+=move;
+			list.remove(minIdx);
+		}
+		System.out.println(dist);
 	}
 
-	private static void bfs(int x, int y, int cnt) {
+	private static void bfs(int fishR, int fishC,int idx) {
 		Queue<Point> que = new LinkedList<Point>();
-		v[0][x][y] = true;
-		que.add(new Point(x, y, cnt, 0, 0));
+		v[sharkRN][sharkCN]=true;
+		que.add(new Point(sharkRN, sharkCN, sharkSize, 0));
 		while (!que.isEmpty()) {
 			Point p = que.poll();
-//			System.out.println("현재 먹은 물고기 수 :"+p.eat);
-			if (fish == p.eat) {
-				Ans = Math.min(Ans, p.cnt);
-			}
-			if (p.eat == babySize) {
-				babySize++;
+			if (p.r == fishR && p.c == fishC) {
+				if (minCnt > p.cnt) {
+					minCnt = p.cnt;
+					minR = p.r;
+					minC = p.c;
+					move = p.cnt;
+					minIdx = idx;
+				} else if (minCnt == p.cnt) {
+					if (minR == p.r) {
+						if (minC > p.c) {
+							move = p.cnt;
+							minR = p.r;
+							minC = p.c;
+							minIdx = idx;
+						}
+					} else if (minR > p.r) {
+						minR = p.r;
+						minC = p.c;
+						move = p.cnt;
+						minIdx = idx;
+					}
+				}
+				return;
 			}
 			for (int k = 0; k < 4; k++) {
 				int nr = p.r + dr[k];
 				int nc = p.c + dc[k];
-				if (nr >= 0 && nr < N && nc >= 0 
-						&& nc < N && !v[p.visit][nr][nc] 
-								&& map[nr][nc] <= babySize) {
-					if (map[nr][nc] < babySize) {// 물고기를 먹으면
-						if (map[nr][nc] != 0) {// 0이아니면 물고기 이므로 먹는다.
-							v[p.visit + 1][nr][nc] = true;
-							System.out
-									.println("물고기 크기(" + babySize + ") " + (nr) + "," + (nc) + "의 물고기를 먹는다." + (p.visit+1));
-//							map[nr][nc] = 0;
-							que.add(new Point(nr, nc, p.cnt + 1, p.eat + 1, p.visit + 1));
-						} else {// 0이면 물고기가 없으므로 그대로 통과
-							v[p.visit][nr][nc] = true;
-							System.out.println("물고기 크기:(" + babySize + ") " + (nr) + "," + (nc) + "는 물고기가 없으므로 패스."+p.visit);
-							que.add(new Point(nr, nc, p.cnt + 1, p.eat, p.visit));
-						}
-					} else {// 물고기를 안먹을 경우
-						System.out.println("물고기 크기:(" + babySize + ") " + (nr) + "," + (nc) + "같은 크기이므로 패스."+p.visit);
-						v[p.visit][nr][nc] = true;
-						que.add(new Point(nr, nc, p.cnt + 1, p.eat, p.visit));
-					}
+				if (nr >= 0 && nr < N && nc >= 0 && nc < N && map[nr][nc] <=sharkSize && !v[nr][nc]) {
+					v[nr][nc] = true;
+					que.add(new Point(nr, nc, sharkSize, p.cnt + 1));
 				}
 			}
 		}
+
 	}
+
 }
