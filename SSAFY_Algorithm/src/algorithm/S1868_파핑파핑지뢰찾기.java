6acc7b0e79282fb28925bdfb460e5d45625e4ad7
koyuchang <go95305@naver.com>
2020-11-06 22:24:32 +0900

    hw_201106

--- SSAFY_Algorithm/src/algorithm/S1868_파핑파핑지뢰찾기.java
+++ SSAFY_Algorithm/src/algorithm/S1868_파핑파핑지뢰찾기.java
@@ -31,47 +31,61 @@ public class S1868_파핑파핑지뢰찾기 {
 			list = new ArrayList<Point>();
 			v = new boolean[N][N];
 			map = new char[N][N];
-			Ans = new int[N][N];
 			for (int i = 0; i < N; i++) {
 				String tmp = sc.next();
 				for (int j = 0; j < N; j++) {
 					map[i][j] = tmp.charAt(j);
-					if (map[i][j] == '*') {
-						list.add(new Point(i, j));
-					}
 				}
 			}
 
-			for (int i = 0; i < list.size(); i++) {
-				bfs(list.get(i).r, list.get(i).c);
-//				check
+			int Ans = 0;
+			for (int i = 0; i < N; i++) {
+				for (int j = 0; j < N; j++) {
+					if (map[i][j] != '*' && !v[i][j]) {
+						if (chk(i, j)) {
+							bfs(i, j);
+							Ans++;
+						}
+					}
+				}
 			}
+			for (int i = 0; i < N; i++) {
+				for (int j = 0; j < N; j++) {
+					if (map[i][j] != '*' && !v[i][j])
+						Ans++;
+				}
+			}
+			System.out.printf("#%d %d\n", tc, Ans);
 		}
 	}
 
+	private static boolean chk(int i, int j) {
+		for (int k = 0; k < 8; k++) {
+			int nr = i + dr[k];
+			int nc = j + dc[k];
+			if (nr >= 0 && nr < N && nc >= 0 && nc < N) {
+				if (map[nr][nc] == '*')
+					return false;
+			}
+		}
+		return true;
+	}
+
 	private static void bfs(int r, int c) {
-		int cnt = 0;
 		Queue<Point> que = new LinkedList<Point>();
 		que.add(new Point(r, c));
+		v[r][c] = true;
 		while (!que.isEmpty()) {
 			Point p = que.poll();
 			for (int k = 0; k < 8; k++) {
 				int nr = p.r + dr[k];
 				int nc = p.c + dc[k];
-				if (nr >= 0 && nr < N && nc >= 0 && nc < N && map[nr][nc] == '*') {
-					cnt++;
-				}
-			}
-			if (cnt == 0) {
-				for (int k = 0; k < 8; k++) {
-					int nr = p.r + dr[k];
-					int nc = p.c + dc[k];
-					if (nr >= 0 && nr < N && nc >= 0 && nc < N && map[nr][nc] != '*' && !v[nr][nc]) {
+				if (nr >= 0 && nr < N && nc >= 0 && nc < N && !v[nr][nc] && map[nr][nc] != '*') {
+					v[nr][nc] = true;
+					if (chk(nr, nc)) {
 						que.add(new Point(nr, nc));
 					}
 				}
-			} else {
-				v[r][c] = true;
 			}
 		}
 	}
--- SSAFY_Algorithm/src/algorithm/S5643_키순서.java
+++ SSAFY_Algorithm/src/algorithm/S5643_키순서.java
@@ -5,9 +5,9 @@ import java.util.Scanner;
 
 public class S5643_키순서 {
 	static int N, M;
-	static ArrayList<Integer>[] list;
+	static ArrayList<Integer>[] list, list2;
 	static int frontSum;
-	static int backSum;
+	static boolean v[];
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
@@ -16,41 +16,42 @@ public class S5643_키순서 {
 			N = sc.nextInt();
 			M = sc.nextInt();
 			list = new ArrayList[N];
+			list2 = new ArrayList[N];
 			for (int i = 0; i < N; i++) {
 				list[i] = new ArrayList<Integer>();
+				list2[i] = new ArrayList<Integer>();
 			}
 
 			for (int i = 0; i < M; i++) {
 				int from = sc.nextInt() - 1;
 				int to = sc.nextInt() - 1;
 				list[from].add(to);
+				list2[to].add(from);
 			}
 			int ans = 0;
 
 			for (int i = 0; i < N; i++) {
+				v = new boolean[N];
 				frontSum = 0;
-				backSum = 0;
-				dfs_before(0, 0);
-				dfs_after(i, 0);
-				if (N - (frontSum + backSum) == 1)
+				dfs_after(list, i, 0, i);
+				v = new boolean[N];
+				dfs_after(list2, i, 0, i);
+				if (frontSum == N - 1)
 					ans++;
 			}
+			System.out.printf("#%d %d\n", tc, ans);
 		}
 	}
 
-	private static void dfs_after(int idx, int cnt) {
-		if (list[idx].size() == 0) {
-			frontSum += cnt;
-			return;
-		}
-
-		for (int i = 0; i < list[idx].size(); i++) {
-			Integer n = list[idx].get(i);
-			dfs_after(n, cnt + 1);
+	private static void dfs_after(ArrayList<Integer>[] lst, int idx, int cnt, int start) {
+		if (idx != start)
+			frontSum++;
+		v[idx] = true;
+		for (int i = 0; i < lst[idx].size(); i++) {
+			Integer n = lst[idx].get(i);
+			if (!v[n])
+				dfs_after(lst, n, cnt + 1, start);
 		}
 	}
 
-	private static void dfs_before(int idx, int cnt) {
-//		if
-	}
 }
