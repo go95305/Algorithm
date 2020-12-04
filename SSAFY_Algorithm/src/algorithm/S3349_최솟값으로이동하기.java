d1c4b5a42a481e516dcf894783b6414ad11cf942
고유창 <go95305@naver.com>
2020-12-04 19:53:28 +0900

    20/12/04

---
+++ SSAFY_Algorithm/src/algorithm/S3349_최솟값으로이동하기.java
@@ -0,0 +1,73 @@
+package algorithm;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class S3349_최솟값으로이동하기 {
+    static int W, H, N;
+    static ArrayList<Point> list;
+
+    static class Point {
+        int r, c;
+
+        Point(int r, int c) {
+            this.r = r;
+            this.c = c;
+        }
+
+        @Override
+        public String toString() {
+            return "Point{" +
+                    "r=" + r +
+                    ", c=" + c +
+                    '}';
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(in.readLine());
+        for (int tc = 1; tc <= T; tc++) {
+            StringTokenizer st = new StringTokenizer(in.readLine(), " ");
+            W = Integer.parseInt(st.nextToken());
+            H = Integer.parseInt(st.nextToken());
+            N = Integer.parseInt(st.nextToken());
+
+            int stx = 0, sty = 0;
+            list = new ArrayList<Point>();
+            for (int i = 0; i < N; i++) {
+                st = new StringTokenizer(in.readLine(), " ");
+                int x = Integer.parseInt(st.nextToken());
+                int y = Integer.parseInt(st.nextToken());
+                list.add(new Point(x, y));
+            }
+//            System.out.println(list);
+            int ans = 0;
+            for (int i = 1; i < list.size(); i++) {
+                Point n1 = list.get(i - 1);
+                Point n2 = list.get(i);
+
+                int x_Cross = (n2.r - n1.r);
+                int y_Cross = (n2.c - n1.c);
+//                System.out.println(is_Cross);
+                if (x_Cross * y_Cross > 0) {
+                    if (Math.abs(x_Cross) > Math.abs(y_Cross)) {
+                        ans += Math.abs(y_Cross) + (Math.abs(x_Cross) - Math.abs(y_Cross));
+                    } else {
+                        ans += Math.abs(x_Cross) + (Math.abs(y_Cross) - Math.abs(x_Cross));
+                    }
+                } else {
+                    ans += Math.abs(n2.r - n1.r) + Math.abs(n2.c - n1.c);
+                }
+            }
+//            System.out.println(ans);
+            System.out.println("#" + tc + " " + ans);
+        }
+    }
+
+
+}
