9852e255e01c094ed3cebeb55d7c1874b4011127
KYC <go95305@naver.com>
2021-07-25 09:13:45 +0900

    21.07.25 백준 쉬운 최단거리

---
+++ SSAFY_Algorithm/src/algorithm/B14940_쉬운최단거리.java
@@ -0,0 +1,73 @@
+package algorithm;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class B14940_쉬운최단거리 {
+    static class Point {
+        int r, c, cnt;
+
+        Point(int r, int c, int cnt) {
+            this.r = r;
+            this.c = c;
+            this.cnt = cnt;
+        }
+    }
+
+    static boolean v[][];
+    static int[] dr = {-1, 1, 0, 0};
+    static int[] dc = {0, 0, -1, 1};
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int map[][] = new int[n][m];
+        Queue<Point> que = new LinkedList<>();
+        v = new boolean[n][m];
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            for (int j = 0; j < m; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+                if (map[i][j] == 2) {
+                    que.add(new Point(i, j, 0));
+                    map[i][j] = 0;
+                    v[i][j] = true;
+                }
+            }
+        }
+        while (!que.isEmpty()) {
+            Point p = que.poll();
+            for (int k = 0; k < 4; k++) {
+                int nr = p.r + dr[k];
+                int nc = p.c + dc[k];
+                if (nr >= 0 && nr < n && nc >= 0 && nc < m && !v[nr][nc] && map[nr][nc] == 1) {
+                    v[nr][nc] = true;
+                    map[nr][nc] = p.cnt + 1;
+                    que.add(new Point(nr, nc, p.cnt + 1));
+                }
+            }
+        }
+        //방문이 불가능한 곳은 -1로 설정
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (map[i][j] == 1 && !v[i][j])
+                    map[i][j] = -1;
+            }
+        }
+
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                System.out.print(map[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
